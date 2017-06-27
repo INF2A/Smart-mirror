9714707601701ee3c81ed1b637275798e3ca444f
Rickbieze <rickbieze@hotmail.com>
2017-06-27 12:56:10 +0200

    queries toegevoeged

--- src/com/smartmirror/sys/DB.java
+++ src/com/smartmirror/sys/DB.java
@@ -34,9 +34,15 @@ public class DB {
     private DB() {}
 
 
-    public void query_test(){
+    public void query_test() throws SQLException {
      //   DB.getConnection();
+
+        //connect.selectQuery("SELECT * FROM news_pref_item");// select alles van news_pref_item
         //connect.selectQuery("SELECT * FROM time_zones");// select alles van tijdzones
+        //connect.selectQuery("SELECT * FROM channel");// select alles van channel
+        //connect.selectQuery("SELECT * FROM time_zones where time_zone LIKE '% input %'" );// select alles van channel
+
+        //connect.selectQuery("SELECT * FROM wifi_settings WHERE User_ID =" + id);//select wifi informatie op basis van user id
         //connect.selectQuery("SELECT time_zone FROM time_zones INNER JOIN time ON time_zones.ID = time.Timezone_ID WHERE time.User_ID = " + id);// select tijdzone van de gebruiker
         //connect.selectQuery("SELECT Name FROM weather_pref INNER JOIN weather ON weather_pref.ID = weather.Weather_pref_ID WHERE weather.User_ID =" + id);//select metric van weer pref van de gebruiker
         //connect.selectQuery("SELECT location FROM weather WHERE User_ID =" +id);//select de locatie van het weer
@@ -44,15 +50,24 @@ public class DB {
         //connect.selectQuery("SELECT Name FROM channel INNER JOIN radio_fav ON channel.ID = radio_fav.Channel_ID WHERE radio_fav.User_ID =" +id);//select de favorite radio channels van de user
         //connect.selectQuery("SELECT * FROM wifi_settings WHERE User_ID ="+id);//select wifi instellingen
         //connect.selectQuery("select * from user where Password='"+ pw +"' AND Username='"+username+"'");//gebruik dit om in te loggen. check of de user en pass correct zijn.
-     //   DB.deleteQuery("DELETE FROM `news_pref` WHERE `User_ID`= "+ id);//delete news pref items van de user
-    }
+        //DB.deleteQuery("DELETE FROM `news_pref` WHERE `User_ID`= "+ id);//delete news pref items van de user
+        //query("INSERT INTO `news_pref`(`User_ID`, `News_pref_item_ID`) VALUES ("+id  +" ,item"); //insert nieuwe news pref item
+        //query("UPDATE radio_fav c set User_ID = "+ id +", Channel_ID =  channel");//insert radio favs
+        //query("UPDATE time set User_ID ="+  id + ", Timezone_ID = timezoneID");//insert nieuwe timezone
+        //query("UPDATE weather SET User_ID =" +id+ ", location = ' location_hier', Weather_pref_ID = pref_id_hier");//insert nieuwe weather location
+        //query("UPDATE wifi_settings set User_ID = "+ id +", ssid = 'ssid hier', password = 'passwoord hier'");//set nieuwe wifi settings
+
+
+
+
+  }
 
     public static void selectQuery(String query) throws SQLException {
         Statement stmt = connection.createStatement();
         stmt.execute(query);
         ResultSet rs = stmt.getResultSet();
         ResultSetMetaData rsm = rs.getMetaData();
-        feed = new ArrayList<String>();
+        feed = new ArrayList<>();
         for (int y = 1; y < rsm.getColumnCount(); y++) {
             feed.add(rsm.getColumnName(y));
         }
